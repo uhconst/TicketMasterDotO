d1cf6b73cb68eeb31847baa595f5a1dec8ac286c
uhconst <uryelhenrique.c@gmail.com>
2025-07-15 10:46:48 +0100

    Create data, domain and presentation modules

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,20 +1,17 @@
 plugins {
-    alias(libs.plugins.android.application)
+    alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
 }
 
 android {
-    namespace = "com.uhc.ticketmasterdoto"
+    namespace = "com.uhc.presentation"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.uhc.ticketmasterdoto"
         minSdk = 29
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -33,9 +30,6 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
-    buildFeatures  {
-        viewBinding = true
-    }
 }
 
 dependencies {
@@ -43,11 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Ticket Master DotO"
 include(":app")
- 
+include(":data")
+include(":domain")
+include(":presentation")
